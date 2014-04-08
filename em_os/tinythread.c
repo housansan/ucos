8eb34a2290cc2d34d4fc540c18b343f0a4159a23
housansan <housansan@yeah.net>
2014-04-08 22:02:23 +0800

    cycle thread run

---
+++ em_os/tinythread.c
@@ -0,0 +1,237 @@
+/*
+ * memcopy(ucp, &uc, sizeof(uc));
+ *
+ * setcontext(ucp);
+ * 1. 使用 stk 保存 uc
+ *  出错原因: stack size 小了
+ *
+ * 2. 增加 ualarm
+ */
+
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define __USE_GNU
+#include <ucontext.h> 
+
+
+#include "tinythread.h"
+
+
+static struct task tsk[TASK_NUM];
+static struct task *cur_tsk;
+static int tsk_thread_id;
+
+static char tsk_stk1[TASK_STK_SIZE];
+static char tsk_stk2[TASK_STK_SIZE];
+static char tsk_stk3[TASK_STK_SIZE];
+
+
+static sigset_t cpu_sr;
+
+
+void delay(int n)
+{
+	int i = 0;
+	for (i = 0; i < n; ++i)
+	{
+	}
+}
+
+
+void tsk_fn1(void)
+{
+	int cnt = 0;
+	while(1) 
+	{
+		ENTER_CRITICAL();
+		printf("in the %s cnt = %d\n", __func__, cnt);
+		EXIT_CRITICAL();
+		// ...
+		delay(N);
+
+		ENTER_CRITICAL();
+		printf("come back %s cnt = %d\n", __func__, cnt);
+		EXIT_CRITICAL();
+		cnt++;
+	}
+}
+
+
+void tsk_fn2(void)
+{
+	int cnt = 0;
+	while(1) 
+	{
+		printf("in the %s cnt = %d\n", __func__, cnt);
+
+		delay(N);
+
+		printf("come back %s cnt = %d\n", __func__, cnt);
+		cnt++;
+	}
+}
+
+
+void tsk_fn3(void)
+{
+	int cnt = 0;
+	while(1) 
+	{
+		printf("in the %s cnt = %d\n", __func__, cnt);
+
+		delay(N);
+
+		printf("come back %s cnt = %d\n", __func__, cnt);
+
+		cnt++;
+	}
+}
+
+
+char *task_stk_init(tsk_fn func, char *ptos)
+{
+	ucontext_t uc;
+	char *stk;
+	int sigsize;
+
+	sigsize = 20 + sizeof(uc);
+	stk = ptos - sigsize;
+
+	getcontext(&uc);
+	uc.uc_link = NULL;
+	uc.uc_mcontext.gregs[REG_EBP] = (int)stk;
+	uc.uc_stack.ss_sp = (void *)(stk - TASK_DEF_STK_SIZE + sigsize);
+	uc.uc_stack.ss_size = TASK_DEF_STK_SIZE - sigsize;
+	makecontext(&uc, func, 0);
+
+	memcpy(stk, &uc, sizeof(uc));
+
+	return stk;
+}
+
+
+int find_free_task(void)
+{
+	int i = 0;
+	for (i = 0; i < TASK_NUM; ++i)
+	{
+		if (0 == tsk[i].used)
+		{
+			break;
+		}
+	}
+
+	return i;
+}
+
+
+int find_next_task(void)
+{
+	tsk_thread_id++;
+	tsk_thread_id = tsk_thread_id % TASK_NUM;
+	return tsk_thread_id;
+}
+
+
+int task_create(tsk_fn func, char *ptos)
+{
+	struct task *ptsk;
+	int priority;
+
+	priority = find_free_task();
+
+	tsk[priority].used = 1;
+	ptsk = &tsk[priority];
+
+	ptsk->stk = task_stk_init(func, ptos);
+
+	return 0;
+}
+
+
+void start_task(void)
+{
+	ucontext_t *ucp;
+	tsk_thread_id = 0;
+	cur_tsk = &tsk[tsk_thread_id];
+
+	ucp = (ucontext_t *)cur_tsk->stk;
+
+	setcontext(ucp);
+}
+
+
+/*
+ * 1. save current ucontext
+ * 2. 
+ */
+void time_tick_sig_handler(int signo, siginfo_t *info, void *uc)
+{
+	ucontext_t *ucp;
+	printf("enter %s\n", __func__);
+	
+	if ((((ucontext_t*)uc)->uc_mcontext.gregs[REG_EIP] >= (unsigned int)setcontext) &&
+			(((ucontext_t*)uc)->uc_mcontext.gregs[REG_EIP] < (unsigned int)(setcontext + 110))) {
+		fprintf(stderr, "sig timer: thread interrupted in setcontext\n");
+		return;
+	}
+	if (((ucontext_t*)uc)->uc_mcontext.fpregs == 0) {
+		fprintf(stderr, "sig timer: uc->uc_mcontext.fpregs == 0\n");
+		return;
+	}
+
+	cur_tsk->stk = uc;
+
+	tsk_thread_id = find_next_task();
+	
+	cur_tsk = &tsk[tsk_thread_id];
+
+	 ucp = (ucontext_t *)cur_tsk->stk;
+
+	setcontext(ucp);
+}
+
+
+void ctx_sw_sig_handler(int signo, siginfo_t *info, void *uc)
+{
+}
+
+
+void sig_init(void)
+{
+	struct sigaction act;
+	act.sa_flags = SA_SIGINFO;
+	sigemptyset(&act.sa_mask);
+	act.sa_sigaction = time_tick_sig_handler;
+	sigaction(SIGALRM, &act, NULL);
+
+	act.sa_flags = SA_SIGINFO;
+	sigemptyset(&act.sa_mask);
+	act.sa_sigaction = ctx_sw_sig_handler;
+	sigaction(SIGUSR1, &act, NULL);
+}
+
+
+void alrm_init(void)
+{
+	useconds_t usec = 100000;
+	ualarm(usec, usec);
+}
+
+
+int main(int argc, char *argv[])
+{
+	sig_init();
+	task_create(tsk_fn1, &tsk_stk1[TASK_STK_SIZE - 1]);
+	task_create(tsk_fn2, &tsk_stk2[TASK_STK_SIZE - 1]);
+	task_create(tsk_fn3, &tsk_stk3[TASK_STK_SIZE - 1]);
+
+	alrm_init();
+	start_task();
+
+	return 0;
+}
