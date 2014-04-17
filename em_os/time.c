69d3745dd4e80779acc2e283875758685ba82764
housansan <housansan@yeah.net>
2014-04-17 22:42:11 +0800

    add time_dly

---
+++ em_os/time.c
@@ -0,0 +1,19 @@
+#include "emos.h"
+
+/*
+ * 目的:
+ */
+void time_dly(int ticks)
+{
+	if (ticks > 0)
+	{
+		ENTER_CRITICAL();
+
+		tcb_exit_rdy(cur_tcb->prio);
+		cur_tcb->delay = ticks;
+
+		EXIT_CRITICAL();
+
+//		scheduler();
+	}
+}
